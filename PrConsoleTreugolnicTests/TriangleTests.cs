3d089c577de7d8befef237bd6d5011bf691ec1fc
Парада Сергей Сергеевич <106469820+ParadaSergei@users.noreply.github.com>
2023-02-13 20:04:58 +0300

    Update TriangleTests.cs

--- PrConsoleTreugolnicTests/TriangleTests.cs
+++ PrConsoleTreugolnicTests/TriangleTests.cs
@@ -18,26 +18,31 @@ namespace PrConsoleTreugolnic.Tests
             Triangle triangle = new Triangle();
             triangle.MetodTriangle(-1, -3, -6, -3, -4, -8);
         }
+        [TestMethod()]
         public void MetodTriangle_Test_To_Check_Positive_Numbers()
         {
             Triangle triangle = new Triangle();
             triangle.MetodTriangle(1, 3, 6, 3, 4, 8);
         }
+        [TestMethod()]
         public void MetodTriangle_Test_To_Check_Positiveand_Negative_Numbers()
         {
             Triangle triangle = new Triangle();
             triangle.MetodTriangle(-1, -3, -6, 3, 4, 8);
         }
+        [TestMethod()]
         public void MetodTriangle_Verification_Test_Floating_Point_Numbers()
         {
             Triangle triangle = new Triangle();
             triangle.MetodTriangle(9.6, 2.4, 6.5, 3.6, 4.1, 8.7);
         }
+        [TestMethod()]
         public void MetodTriangle_Verification_Test_Negative_Numbers_Floating_Point()
         {
             Triangle triangle = new Triangle();
             triangle.MetodTriangle(-9.6, -2.4, -6.5, -3.6, -4.1, -8.7);
         }
+        [TestMethod()]
         public void MetodTriangle_Verification_Test_Floating_Point_Numbers_And_Denial_Floating_Point()
         {
             Triangle triangle = new Triangle();
